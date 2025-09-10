e5851aa91981a51edae8a67a7e7fe0432a55ffd8
Ahmad Hassan <ahmadrizvi.dev@gmail.com>
2025-09-10 18:19:20 +0500

    fix ui

--- componenets/Collections.jsx
+++ componenets/Collections.jsx
@@ -7,8 +7,8 @@ import { motion } from "framer-motion";
 const collections = [
   {
     title: "Mens",
-    image: "https://plus.unsplash.com/premium_photo-1691030256214-dc57034ec935?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    href: "/collections/men",
+    image: "/assets/men.jpg",
+    href: "/collections/mens-winter-collection",
   },
   {
     title: "Womens",
