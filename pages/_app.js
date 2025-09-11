7a52a5df5abd3f226cee36639d27e12e0599a7d0
Ahmad Hassan <ahmadrizvi.dev@gmail.com>
2025-09-11 20:27:33 +0500

    fix ui

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,24 @@ import Header from "../componenets/Navbar.jsx";
 import AnnouncementBar from "@/componenets/Announmentbar.jsx";
 import Footer from "@/componenets/footer.jsx";
 import WhatsappButton from "@/componenets/WhatsappButton.jsx";
+import { useRouter } from "next/router.js";
+import * as gtag from "../lib/gtag";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <>
   <AnnouncementBar/>
     <Header/>
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const GA_TRACKING_ID = "G-MGZ8GZ6Y8T"; // replace with your GA4 Measurement ID
+
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
